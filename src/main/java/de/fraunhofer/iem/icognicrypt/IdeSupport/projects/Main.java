184316343d47d4d8b8e8c240672af95088fd76d2
Sebastian Leuer <sebastian.leuer@iem.fraunhofer.de>
2019-06-18 10:23:30 +0200

    simplified

--- src/main/java/de/fraunhofer/iem/icognicrypt/IdeSupport/projects/Main.java
+++ src/main/java/de/fraunhofer/iem/icognicrypt/IdeSupport/projects/Main.java
@@ -1,6 +1,5 @@
 package de.fraunhofer.iem.icognicrypt.IdeSupport.projects;
 
-import de.fraunhofer.iem.icognicrypt.IdeSupport.gradle.GradleSettings;
 import de.fraunhofer.iem.icognicrypt.exceptions.CogniCryptException;
 
 import javax.naming.OperationNotSupportedException;
@@ -8,26 +7,12 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 
-
 public class Main
 {
-
     public static void main(String[] args) throws IOException, OperationNotSupportedException, CogniCryptException
     {
-        // TODO: Enable
-        // System.out.print("Please enter the root path to your Android App project:");
-        // var s = new Scanner(System.in);
-        // var path = s.nextLine();
         String tmpPath = "C:\\Users\\lrs\\AndroidStudioProjects\\HelloWorld";
-        //Configuration.GetInstance().SetProjectPath(tmpPath);
-
-        //Path projectPath = Paths.get(Configuration.GetInstance().GetProjectPath());
-
-        GradleSettings settings = new GradleSettings(Paths.get(tmpPath));
-
         IOutputFinder outputFinder = AndroidStudioOutputFinder.GetInstance();
-
-
         Iterable<File> files = outputFinder.GetOutputFiles(Paths.get(tmpPath), OutputFinderOptions.AnyBuildType);
         System.out.println(files);
         while (true){

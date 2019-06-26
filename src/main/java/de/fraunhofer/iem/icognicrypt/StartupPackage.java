ed0f9fb741c28192e94b725094a7b19705c5161a
Sebastian Leuer <sebastian.leuer@iem.fraunhofer.de>
2019-06-26 15:15:06 +0200

    stuff

--- src/main/java/de/fraunhofer/iem/icognicrypt/StartupPackage.java
+++ src/main/java/de/fraunhofer/iem/icognicrypt/StartupPackage.java
@@ -1,13 +1,7 @@
 package de.fraunhofer.iem.icognicrypt;
 
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.components.BaseComponent;
 import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.progress.Task;
-import org.jetbrains.annotations.NotNull;
-import sun.util.resources.cldr.om.CalendarData_om_ET;
+import de.fraunhofer.iem.icognicrypt.core.BackgroundPackage;
 
 /**
  * This class is the main entry point to CogniCrypt. When a project is loaded after the IDE was started we consider the IDE setup to be completed.
@@ -19,16 +13,15 @@ public class StartupPackage extends BackgroundPackage
 {
     public StartupPackage(){
         Title = "Test123";
+        CanCancelInit = true;
     }
 
     @Override
-    protected void InitializeAsync(ProgressIndicator indicator)
+    protected void InitializeInBackground(ProgressIndicator indicator)
     {
         System.out.println("starting");
         indicator.setIndeterminate(true);
-        indicator.setText("123");
-        indicator.setText2("456");
-        for (int i = 0 ; i < 100; i++){
+        for (int i = 0 ; i < 10; i++){
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -41,52 +34,4 @@ public class StartupPackage extends BackgroundPackage
     }
 }
 
-class BackgroundPackage implements BaseComponent
-{
-    protected String Title = "";
 
-    @Override
-    public final void initComponent()
-    {
-        ProgressManager.getInstance().run(new BTask(this));
-    }
-
-    protected void InitializeAsync(ProgressIndicator indicator){
-
-    }
-
-    protected void OnCanceled(){
-
-    }
-
-    private class BTask extends Task.Backgroundable{
-
-        private BackgroundPackage _owner;
-
-        public BTask(BackgroundPackage owner)
-        {
-            super(null, owner.Title, true);
-            _owner =owner;
-        }
-
-        @Override
-        public void run(@NotNull ProgressIndicator indicator)
-        {
-            _owner.InitializeAsync(indicator);
-        }
-
-        @Override
-        public void onFinished()
-        {
-            super.onFinished();
-            _owner = null;
-        }
-
-        @Override
-        public void onCancel()
-        {
-            super.onCancel();
-            _owner.OnCanceled();
-        }
-    }
-}
---
+++ src/main/java/de/fraunhofer/iem/icognicrypt/core/BackgroundPackage.java
@@ -0,0 +1,71 @@
+package de.fraunhofer.iem.icognicrypt.core;
+
+import com.intellij.openapi.components.BaseComponent;
+import com.intellij.openapi.progress.PerformInBackgroundOption;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Base class for Packages that shall run in Background. A package is considered to be an instance that contains routines to setup services which will
+ * be used by the plugin.
+ *
+ * NOTE: When running a {@link BackgroundPackage} as a application-component pushing the modal dialog into background causes it to disappear from the
+ * background task window of the IDE.
+ */
+public class BackgroundPackage implements BaseComponent
+{
+    protected String Title = "";
+    protected boolean CanCancelInit = true;
+
+
+    @Override
+    public final void initComponent()
+    {
+        // Commented code does not create any dialog but i can not get it to display if I'd want to.
+        // ProgressIndicator indicator = ProgressManager.getInstance().getProgressIndicator();
+        // ProgressManager.getInstance().runProcessWithProgressAsynchronously(new PackageTask(this), indicator);
+        ProgressManager.getInstance().run(new PackageTask(this));
+    }
+
+    protected void InitializeInBackground(ProgressIndicator indicator)
+    {
+    }
+
+    protected void OnCanceled()
+    {
+    }
+
+    private class PackageTask extends Task.Backgroundable
+    {
+
+        private BackgroundPackage _owner;
+
+        public PackageTask(BackgroundPackage owner)
+        {
+            super(null, owner.Title, owner.CanCancelInit);
+            _owner = owner;
+        }
+
+        @Override
+        public void run(@NotNull ProgressIndicator indicator)
+        {
+            _owner.InitializeInBackground(indicator);
+        }
+
+        @Override
+        public void onFinished()
+        {
+            super.onFinished();
+            _owner = null;
+        }
+
+        @Override
+        public void onCancel()
+        {
+            super.onCancel();
+            _owner.OnCanceled();
+        }
+    }
+}

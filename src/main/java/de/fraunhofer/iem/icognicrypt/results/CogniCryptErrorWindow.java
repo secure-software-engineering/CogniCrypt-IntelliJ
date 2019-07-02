b91622b5ac70a4ee5a3e2ff423271771dda59c65
Sebastian Leuer <sebastian.leuer@iem.fraunhofer.de>
2019-07-02 13:38:12 +0200

    added demo table

--- src/main/java/de/fraunhofer/iem/icognicrypt/results/CogniCryptErrorWindow.java
+++ src/main/java/de/fraunhofer/iem/icognicrypt/results/CogniCryptErrorWindow.java
@@ -9,11 +9,26 @@ public class CogniCryptErrorWindow extends CogniCryptWindowBase
 {
     private JPanel _content;
     private JTextField _textField1;
-    private JTable table1;
+    private JTable _errorTable;
 
     public CogniCryptErrorWindow(ToolWindow toolWindow)
     {
         super(toolWindow);
+
+
+        Object[][] data = {
+                {"Kathy", "Smith",
+                        "Snowboarding", new Integer(5), new Boolean(false)},
+                {"John", "Doe",
+                        "Rowing", new Integer(3), new Boolean(true)},
+                {"Sue", "Black",
+                        "Knitting", new Integer(2), new Boolean(false)},
+                {"Jane", "White",
+                        "Speed reading", new Integer(20), new Boolean(true)},
+                {"Joe", "Brown",
+                        "Pool", new Integer(10), new Boolean(false)}
+        };
+
     }
 
     @Override

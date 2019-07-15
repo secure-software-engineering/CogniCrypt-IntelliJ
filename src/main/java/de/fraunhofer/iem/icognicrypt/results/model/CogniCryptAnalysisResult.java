48a74191d1f8c7979597762c0fb7a5a97db7f41d
Sebastian Leuer <sebastian.leuer@iem.fraunhofer.de>
2019-07-15 09:47:10 +0200

    prepared minimal error type data model

--- src/main/java/de/fraunhofer/iem/icognicrypt/results/model/CogniCryptAnalysisResult.java
+++ src/main/java/de/fraunhofer/iem/icognicrypt/results/model/CogniCryptAnalysisResult.java
@@ -1,18 +1,25 @@
 package de.fraunhofer.iem.icognicrypt.results.model;
 
-import boomerang.jimple.Statement;
+import com.intellij.openapi.util.UserDataHolderBase;
 import crypto.analysis.errors.AbstractError;
+import crypto.analysis.errors.ConstraintError;
+import crypto.interfaces.ISLConstraint;
 import soot.SootMethod;
 import soot.jimple.Stmt;
 
-public class CogniCryptAnalysisResult
+public class CogniCryptAnalysisResult extends UserDataHolderBase
 {
     CogniCryptAnalysisResultLocation _location;
     String _message;
+    CogniCryptErrorCategory _errorType;
 
     public CogniCryptAnalysisResultLocation GetLocation()
     {
         return _location;
+    }
+
+    public CogniCryptErrorCategory GetErrorType(){
+        return _errorType;
     }
 
     public String GetMessage(){
@@ -27,10 +34,21 @@ public class CogniCryptAnalysisResult
 
         Stmt statement = cogniCryptError.getErrorLocation().getUnit().get();
 
+        if (cogniCryptError instanceof ConstraintError){
+            ConstraintError constraintError = (ConstraintError) cogniCryptError;
+
+            ISLConstraint constraint = constraintError.getBrokenConstraint();
+            String value = constraintError.getCallSiteWithExtractedValue().getVal().getValue().toString();
+        }
+
+
         MethodSignature methodSignature = new MethodSignature(affectedMethod);
         CogniCryptAnalysisResultLocation location = new CogniCryptAnalysisResultLocation(affectedClass, methodSignature, line);
         _location = location;
         _message = cogniCryptError.toErrorMarkerString();
+    }
+
+    public static void Test(Object t, Object s){
     }
 
     @Override
@@ -39,7 +57,8 @@ public class CogniCryptAnalysisResult
         if (obj == null) return false;
         if (!(obj instanceof CogniCryptAnalysisResult)) return false;
         CogniCryptAnalysisResult other = (CogniCryptAnalysisResult) obj;
-        return GetLocation().equals(other.GetLocation());
+        boolean sameLocation = GetLocation().equals(other.GetLocation());
+        return sameLocation && _errorType == other.GetErrorType();
     }
 }
 

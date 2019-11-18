b05e62a9e017e22f5cf936b61f0509e69e12cc8b
lrs <sebastian.leuer@iem.fraunhofer.de>
2019-11-18 16:19:58 +0100

    use constant

--- src/main/java/de/fraunhofer/iem/icognicrypt/Constants.java
+++ src/main/java/de/fraunhofer/iem/icognicrypt/Constants.java
@@ -1,13 +1,16 @@
 package de.fraunhofer.iem.icognicrypt;
 
 
+import crypto.cryptslhandler.CrySLModelReader;
+
 public class Constants {
 
     public final static boolean AUTOMATIC_SCAN_ON_COMPILE = false;
 
     public static final String DummyCrySLPath = "./CrySLRules/JavaCryptographicArchitecture";
 
-    public static final String CRYSL_BIN_EXTENSION = ".cryptslbin";
+    // TODO: This constant is not final in CryptoAnalysis
+    public static final String CRYSL_BIN_EXTENSION = CrySLModelReader.cryslFileEnding;
     public static final String CRYSL_RULES_DOWNLOADLINK = "https://github.com/CROSSINGTUD/CryptoAnalysis/releases/download/2.3/CrySL-rulesets.zip";
     public static final boolean WARNINGS_IN_SOURCECODECLASSES_ONLY = true;
 }

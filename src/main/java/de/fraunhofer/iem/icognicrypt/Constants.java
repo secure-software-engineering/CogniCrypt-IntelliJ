5dba1f6667e5c152c488eb50a2ec4a0aea38a365
AnakinSklavenwalker <anakin@gmx-topmail.de>
2020-11-05 13:47:31 +0100

    fix for android studio 4.1 update

--- src/main/java/de/fraunhofer/iem/icognicrypt/Constants.java
+++ src/main/java/de/fraunhofer/iem/icognicrypt/Constants.java
@@ -6,13 +6,13 @@ public class Constants {
 
     public final static boolean AUTOMATIC_SCAN_ON_COMPILE = false;
 
-    public static final String DummyCrySLPath = "./CrySLRules/JavaCryptographicArchitecture";
+    public static final String DummyCrySLPath = "./CrySLRules/JCA";
 
     // TODO: This constant is not final in CryptoAnalysis
     public static final String CRYSL_EXTENSION = CrySLModelReader.cryslFileEnding;
     public static final String CRYSL_RULES_DOWNLOADLINK = "https://soot-build.cs.uni-paderborn.de/nexus/repository/soot-release/de/darmstadt/tu/crossing/JavaCryptographicArchitecture/1.5.1/JavaCryptographicArchitecture-1.5.1-ruleset.zip";
     public static final boolean WARNINGS_IN_SOURCECODECLASSES_ONLY = true;
 
-    public static final String ResourceZipPath = "/CrySLRules/JavaCryptographicArchitecture-1.5.1-ruleset.zip";
+    public static final String ResourceZipPath = "/CrySLRules/JCA1.5.2.zip";
     public static final String DefaultExtractLocation = "./CrySLRules/";
 }
